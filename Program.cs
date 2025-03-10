015fe40580c0c737a2972bc6c7bdccf74da0b4fc
GabrielFI2 <aarongccfi2@gmail.com>
2025-03-10 11:01:32 -0600

    Update Program.cs
    checR INTERFACES

--- Program.cs
+++ Program.cs
@@ -43,3 +43,117 @@ Console.WriteLine(piEntero);
 //Casteo de objetos
 
 //DownC 
+                           //clase base
+class Animal
+{
+    //Metodos 
+    public void Respirar()
+    {
+        Console.WriteLine("EStoy respirando");
+    }
+
+    public virtual  void HacerSonido()
+    {
+        Console.WriteLine("Haace sonido");
+    }
+    interface IAnimalTerreste
+        //lña interfaz solo va decrar
+        //no va estar impementado
+    {
+        public void Caminar()
+        {
+           
+        }
+        //Metodo terrestre
+        
+
+    }
+    interface IAnimalMAnufero
+    {
+        public void mamifero()
+        {
+           // Console.WriteLine("Haace cosas de mamifero");
+        }
+        
+    }
+}
+
+class Perro : Animal , IAnimalTerreste , IAnimalMAnufero
+{
+    //Metod del perro
+    public  override void HacerSonido()
+    {
+        Console.WriteLine("Gua Guau");
+    }
+    public void Caminar()
+    {
+        Console.WriteLine("puede caminar");
+    }
+}
+
+class Gato : Animal
+{
+    //Metod del perro
+    public override void HacerSonido()
+    {
+        Console.WriteLine("Miau Miau");
+    }
+}
+public class Ejemplo : IEjemplo
+{
+    //implementacion del metodo de la interfaz
+
+    public void HacerAlgo()
+    {
+        Console.WriteLine("Hacer algo");
+    }
+  
+}
+IEjemplo algo = new Ejemplo();
+algo.HacerAlgo();
+algo.HacerAlgoMas();
+Perro miPerro = new Perro();
+
+miPerro.Respirar();
+miPerro.HacerSonido();
+
+Animal pajaro = new Pajaro();
+pajaro.V();
+pajaro.Respirar();
+
+Animal miAnimal = new Animal();
+                           public interface IEjemplo
+{
+    //metodos de ejemplo
+    void HacerAlgo()
+    {
+    }
+    //metodo con implementacio perdeterminada
+    public void HacerAlgoMas()
+    {
+        Console.WriteLine("Haciendo otra cosa");
+    }
+}
+//interfaz es una opcipon para hacer apartados un cacho de informacion
+//interface IVolador
+{
+    //Metodo Interfaz
+    public void Volar()
+    {
+        
+    }
+
+}
+
+class Pajaro : Animal , IVolador
+{
+    public void Volar()
+    {
+        Console.WriteLine("Estoy volando");
+    }
+    public override void HacerSonido()
+    {
+        Console.Write("pio pio");
+    }
+
+}
